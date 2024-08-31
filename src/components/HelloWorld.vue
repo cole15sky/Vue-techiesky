5724734804d73893a6578b6257f84866e6c3df34
Sky Blue <dawadiaakash@gmail.com>
2024-08-31 13:17:28 +0545

    Fetch: Vue initialize / Start of site.

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div> 
+    <h1> This project highlights on techiesky.com official project made from Vue and Tailwind.</h1>
+  </div>
+  </template>
+
+<style scoped>
+
+</style>
