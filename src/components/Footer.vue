5f502b13d4745bec6ad9275e74e1ac019d12221e
Sky Blue <dawadiaakash@gmail.com>
2024-09-20 23:25:32 +0545

    Fix(footer): Style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,10 @@
 <template>
     <footer class="bg-white rounded-lg shadow p-4 fixed bottom-0 left-0 w-full dark:bg-gray-800">
       <div class="w-full mx-auto max-w-screen-xl md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Designed and developed by Aakash Dawadi 
-             <span class="items-center justify-center"> © 2024</span> <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <span class="text-sm text-gray-500 sm dark:text-gray-400 ml-0">Designed and developed by Aakash Dawadi 
+             <span class="hover:underline ml-20"> All Rights Reserved © 2024</span> 
+            </span>
+        <ul class="flex flex-wrap text-sm font-medium text-gray-500 dark:text-gray-400 ">
             <li>
             <a href="#" class="hover:underline me-4 md:me-6">Home</a>
           </li>
