323ad121e740040b3e699e09a894c03223a9e838
Sky Blue <dawadiaakash@gmail.com>
2024-09-20 22:39:44 +0545

    Feat(footer): add and design initial footer structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
     
     <div>
-      <HelloWorld />
+      <NavBar />
+      <Footer/>
     </div>
 </template>
 
