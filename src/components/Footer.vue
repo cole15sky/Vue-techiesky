c32d41bf56e4b17f99c9d5e9379c85e773b22028
Sky Blue <dawadiaakash@gmail.com>
2024-09-20 23:15:23 +0545

    Fix(Footer): position as fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,45 +1,35 @@
 <template>
-
-
-<footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Designed and developed by Aakash Dawadi.© 2024 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
-    </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-        <li>
+    <footer class="bg-white rounded-lg shadow p-4 fixed bottom-0 left-0 w-full dark:bg-gray-800">
+      <div class="w-full mx-auto max-w-screen-xl md:flex md:items-center md:justify-between">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">Designed and developed by Aakash Dawadi 
+             <span class="items-center justify-center"> © 2024</span> <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Home</a>
+          </li>
+         
+          <li>
             <a href="#" class="hover:underline me-4 md:me-6">About</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-        </li>
-        <li>
-            <a href="#" class="hover:underline">Contact</a>
-        </li>
-    </ul>
-    </div>
-</footer>
-
-    
-</template>
-
-
-
-
-
-<script >
-export default {
-  name: "Footer",
-};
-
-</script>
-
-
-
-
-
-<style scoped>
-
-</style>
+          </li>
+          <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Gallery</a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline me-4 md:me-6">Article</a>
+          </li>
+         
+        </ul>
+      </div>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: "Footer",
+  };
+  </script>
+  
+  <style scoped>
+  /* Additional styles can go here */
+  </style>
+  
